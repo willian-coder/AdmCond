8f10c401e8b3bc85062ef391128eed9c6e3d03ad
willianlq <willianlq@outlook.com>
2020-07-24 17:44:28 -0300

    Iniciando upload do projeto - ADM cond

---
+++ app/src/main/java/com/projeto/admcond/SuporteActivity.java
@@ -0,0 +1,13 @@
+package com.projeto.admcond;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SuporteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suporte);
+    }
+}

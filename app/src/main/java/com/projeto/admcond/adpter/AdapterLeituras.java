942d19b32db70f325af2302658ee7ba471d9139e
willianlq <willianlq@outlook.com>
2020-07-26 12:55:49 -0300

    ADM Cond - 26/072020

---
+++ app/src/main/java/com/projeto/admcond/adpter/AdapterLeituras.java
@@ -0,0 +1,60 @@
+package com.projeto.admcond.adpter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+
+import com.projeto.admcond.R;
+import com.projeto.admcond.configfirebase.NovoInquilino;
+
+import java.util.List;
+
+public class AdapterMoradores extends RecyclerView.Adapter<AdapterMoradores.MyViewHolder> {
+
+    private List<NovoInquilino> inquilinos;
+    public Context context;
+
+    public AdapterMoradores(List<NovoInquilino> inquilinos, Context context) {
+        this.inquilinos = inquilinos;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_moradores, parent, false);
+        return new MyViewHolder(item);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        NovoInquilino novoInquilino = inquilinos.get(position);
+        holder.apartamento.setText(novoInquilino.getNumeroApt());
+        holder.nomeInquilino.setText(novoInquilino.getNomeInquilino());
+    }
+
+    @Override
+    public int getItemCount() {
+        return inquilinos.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView apartamento;
+        TextView nomeInquilino;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            apartamento = itemView.findViewById(R.id.textApt);
+            nomeInquilino = itemView.findViewById(R.id.textInquilino);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/projeto/admcond/configfirebase/NovaLeitura.java
@@ -0,0 +1,4 @@
+package com.projeto.admcond.configfirebase;
+
+public class NovaLeitura {
+}
